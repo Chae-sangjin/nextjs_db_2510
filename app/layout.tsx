f21fc4a429cc3132d69a5a569d83edf36e3e9469
Chae-sangjin <cotkdwls1@naver.com>
2025-10-16 16:22:49 +0900

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,10 +13,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="ko">
+      <body>
+        <nav>
+          <Link href="/">홈</Link>
+          <Link href="/about">소개</Link>
+          <Link href="/article">게시판</Link>
+        </nav>
         {children}
       </body>
     </html>
