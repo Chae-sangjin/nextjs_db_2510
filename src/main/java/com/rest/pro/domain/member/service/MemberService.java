61c97d3d2372c5836915c00e2286d111f523ddf8
Chae-sangjin <cotkdwls1@naver.com>
2025-10-21 16:05:25 +0900

    sj

--- src/main/java/com/rest/pro/domain/member/service/MemberService.java
+++ src/main/java/com/rest/pro/domain/member/service/MemberService.java
@@ -1,6 +1,6 @@
 package com.rest.pro.domain.member.service;
 
-
+import com.rest.pro.domain.member.entity.Member;
 import com.rest.pro.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,5 +8,17 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-    private final MemberRepository memberrepository;
+    private final MemberRepository memberRepository;
+
+    public Member join(String username, String password, String email) {
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .build();
+
+        memberRepository.save(member);
+
+        return member;
+    }
 }
